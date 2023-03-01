cf800fbf61755806f4b81541b4471744cd5ec398
Matheus Mendes <matheusmendes2005@outlook.com>
2023-03-01 11:43:47 -0300

    Criação da estrutura MVC

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
-    echo "GIT";
+        require __DIR__.'./vendor/autoload.php';
+        use App\Controller\GitController;
+        $git = new GitController();
 ?>
+
+<link rel="stylesheet" href="style.css">
+<div class="meio">
+    <div class="abc">
+        <div class="aaa">
+        <?php
+            echo $git->iniciar();
+        ?>
+        </div>
+        <div class="aaa">
+        <?php
+            echo $git->conta();
+        ?>
+        </div>    
+    </div>
+</div>
